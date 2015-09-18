0272b390328a5dfb0d8cb7a941220ddfbcfe8dac
semdinsp <scott.sproule@ficonab.com>
2015-09-18 13:27:07 +0800

    upgrade to bootstrap marketing theme

--- app.rb
+++ app.rb
@@ -1,4 +1,14 @@
+
+
+
 module Nesta
+  module Navigation
+    module Renderers
+      def current_menu_item_class
+              'active'
+            end
+    end
+  end
   class App
     not_found do
       cache haml("404".to_sym)
